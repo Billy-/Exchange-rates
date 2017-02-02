52bff0e00713c10e05393d39146bef234db518ae
Billy- <billy@billym.co.uk>
2017-02-02 19:07:51 +0000

    Fixed Link style issue

--- app/src/components/header/index.jsx
+++ app/src/components/header/index.jsx
@@ -3,6 +3,6 @@ import CSSModules from 'react-css-modules'
 import { Link } from 'react-router'
 import styles from './style.css'
 
-let Header = _ => <Link style={{"text-decoration": "none"}} to='/'><h1 styleName="header">Exchange-R8s</h1></Link>
+let Header = _ => <Link style={{"textDecoration": "none"}} to='/'><h1 styleName="header">Exchange-R8s</h1></Link>
 
 export default CSSModules(Header, styles)
