4d0c6fc81df92506b5876f8ec7feab82f3e73816
Billy- <billy@billym.co.uk>
2017-02-03 03:32:45 +0000

    Fixed bug with dataset ordering

--- app/src/actions/historyActions.js
+++ app/src/actions/historyActions.js
@@ -9,17 +9,26 @@ export const GET_HISTORY_FAILURE = 'GET_HISTORY_FAILURE'
 export const getHistory = (base, compare) => {
     return dispatch => {
         dispatch({type: GET_HISTORY_SUBMIT, payload: compare})
-        const reqs = [...Array(12).keys()].map(e => moment().subtract(e, 'months')).reverse()
+        const reqs = [...Array(12).keys()].map(e => moment().subtract(e, 'months'))
+        let responses = []
         let data = { labels: reqs.map(d => d.format('MMM')), datasets: [{data: [], fillColor: 'rgba(52,204,204,0.3)', borderColor: 'rgba(52,204,204,1)'}]}
         return Promise.map(reqs, d => {
             return axios.get(`https://api.fixer.io/${d.format('YYYY-MM-DD')}`, { params: { base, symbols: compare } })
-                .then(response => data.datasets[0].data.push(response.data.rates[compare]))
+                .then(response => responses.push(response.data))
         }, { concurrency: 10 })
         .then(_ => {
+            data.datasets[0].data = sortByKey(responses.map(r => { return {date: r.date, rate: r.rates[compare] }}), 'date').map(r => r.rate)
             dispatch({type: GET_HISTORY_SUCCESS, payload: data })
         })
         .catch(error => {
             dispatch({type: GET_HISTORY_FAILURE, payload: error.response ? error.response.data.error : error })
         })
     }
+}
+
+function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
 }
