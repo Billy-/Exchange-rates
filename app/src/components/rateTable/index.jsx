f411656d403ef122154181809ce9889a3b8934d2
Billy- <billy@billym.co.uk>
2017-02-03 04:50:51 +0000

    Remove console.logs

--- app/src/components/rateTable/index.jsx
+++ app/src/components/rateTable/index.jsx
@@ -9,7 +9,6 @@ import History from '../history'
 
 let RateTable = state => {
     const { rates, comparing, onChangeHistory } = state
-    console.log(comparing)
     const keys = Object.keys(rates).sort()
     const ci = keys.indexOf(comparing)
     const tableContent = keys.map(r => {
@@ -19,7 +18,6 @@ let RateTable = state => {
         </tr>
     })
     const history = <MediaQuery key="history" query="(max-width: 800px)">{ ci > -1 ? <tr styleName="history"><td colSpan="2"><History /></td></tr> : null }</MediaQuery>
-    console.log(ci)
     tableContent.splice(ci+1, 0, history )
     return <table styleName='table'>
         <thead><tr><th>Currency</th><th>Rate</th></tr></thead>
